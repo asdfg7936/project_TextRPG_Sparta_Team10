4c133b5e89be638ff90ab7e76d44e1f0a331c527
pddddq <old011003@naver.com>
2025-01-10 15:38:11 +0900

    경험치 추가

--- main/Monster.h
+++ main/Monster.h
@@ -7,13 +7,15 @@ protected:
     std::string name;
     int health;
     int attack;
+    int exp;
 
 public:
-    Monster(std::string name, int health, int attack);
+    Monster(std::string name, int health, int attack, int exp);
     virtual ~Monster() {}
 
     virtual std::string GetName() const = 0;
     virtual int GetHealth() const = 0;
     virtual int GetAttack() const = 0;
+    virtual int GetExp() const = 0;
     virtual void TakeDamage(int damage) = 0;
 };
