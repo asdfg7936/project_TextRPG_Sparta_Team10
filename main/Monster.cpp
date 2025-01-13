9465304b61ca4dc73e8a43b140072e2f80141b81
Gihoon <89381041+Qussong@users.noreply.github.com>
2025-01-13 15:25:27 +0900

    몬스터 클래스 오류 수정

--- main/Monster.cpp
+++ main/Monster.cpp
@@ -1,4 +1,9 @@
 #include "Monster.h"
 
+/*
 Monster::Monster(std::string name, int health, int attack, int exp)
-    : name(name), health(health), attack(attack), exp(exp) {}
+	: name(name), health(health), attack(attack), exp(exp) {}
+*/
+
+Monster::Monster(int level, std::string name, int health, int attack, int exp)
+	: name(name), health(health), attack(attack), exp(exp) {}
